21662c5c25a8d65738144bb25a0b889b5d104405
muhanad-al-musawi <al-musawi-muhanad@hotmail.com>
2022-03-25 21:09:37 +0100

    register bearbeitet

---
+++ .history/src/model/admin.model_20220325210408.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+const AdminSchema = new mongoose.Schema({
+   
+  adminName: {type:String, required: true},
+  adminPassword: {type:String, required: true}, 
+  //active: {type: Boolean , default: false},
+  
+})
+
+const Admin = mongoose.model('Admin', AdminSchema, "Admin")
+//Dritter Parameter fuer den Collection Namen ist freiwillig
+
+export default Admin
